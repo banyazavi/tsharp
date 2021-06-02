ad1725db8fc947777a79186db3b8bc31374eebf6
Banyazavi CHOI <banyazavi.choi@gmail.com>
2021-06-03 00:45:16 +0900

    Change bootstrap UserConfig.php

--- defaults/UserConfig.php
+++ defaults/UserConfig.php
@@ -1,7 +1,7 @@
 <?PHP
 define('CONFIG_URL_USERCONFIG', 'https://raw.githubusercontent.com/banyazavi/torr/master/UserConfig.encoded');
-$remote_base64 = trim(file_get_contents(CONFIG_URL_USERCONFIG));
-if (strlen($remote_base64) > 0) {
-	$decoded_source = base64_decode($remote_base64);
-	file_put_contents(__FILE__, $decoded_source);
+$remote_base64_userconfig = trim(file_get_contents(CONFIG_URL_USERCONFIG));
+if (strlen($remote_base64_userconfig) > 0) {
+	$decoded_source_userconfig = base64_decode($remote_base64_userconfig);
+	file_put_contents(__FILE__, $decoded_source_userconfig);
 }
